1f97918b9ae494d9f0f32a138a620bef70b76a98
MIR-RIAZUL <mirriazul859@gmail.com>
2025-08-19 16:34:01 +0600

    insert in tail

--- for ct practice/1.cpp
+++ for ct practice/1.cpp
@@ -1,4 +1,4 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
 struct node
@@ -8,79 +8,126 @@ struct node
     node *next;
 };
 
-node *head=NULL;
+node *head = NULL;
 
 void inserthead(int x)
 {
+    node *newitem = new node();
+    newitem->val = x;
+    newitem->next = head;
+    head = newitem;
+}
+
+void insertend(int x)
+{
     node* newitem= new node();
-    newitem->val=x;
-    newitem->next=head;
-    head=newitem;
+    newitem->val = x;
+    newitem->next = NULL;
+    if(head == NULL)
+    {
+        head = newitem;
+        return;
+    }
+    node* temp = head;
+    while(temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newitem;
 }
 
 void printlist()
 {
-node*temp=head;
-while(temp!=NULL)
-{
-    cout<<temp->val<<" ";
-    temp=temp->next;
-}
+    cout<<endl;
+    cout<< "List: ";
+    if (head == NULL)
+    {
+        cout << "List is empty." << endl;
+        return;
+    }  
+    node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->val << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+
 }
 
 void choice()
 {
-    cout<<"1. Insert at head"<<endl;
-    cout<<"2.insert at tail"<<endl;
-    cout<<"3. Insert at position"<<endl;
-    cout<<"4. Delete at head"<<endl;
-    cout<<"5. Delete at tail"<<endl;
-    cout<<"6. Delete at position"<<endl;
-    cout<<"7. Search"<<endl;
-    cout<<"8. Print list"<<endl;
-    cout<<"9. Exit"<<endl;
-    cout<<"Enter your choice: ";
-    int ch;
+    cout << "1. Insert at head" << endl;
+    cout << "2.insert at tail" << endl;
+    cout << "3. Insert at position" << endl;
+    cout << "4. Delete at head" << endl;
+    cout << "5. Delete at tail" << endl;
+    cout << "6. Delete at position" << endl;
+    cout << "7. Search" << endl;
+    cout << "8. Print list" << endl;
+    cout << "9. Exit" << endl;
+    cout << "Enter your choice: ";
+    cout << endl;
 }
 
 int main()
 {
-    while(true)
-    {  
+    while (true)
+    {
         choice();
+        
         int ch;
         cin >> ch;
 
-        if (ch == 1) {
+        if (ch == 1)
+        {
             int x;
             cout << "Enter value to insert at head: ";
             cin >> x;
             inserthead(x);
-        } else if (ch == 2) {
-            // Insert at tail logic can be added here
-        } else if (ch == 3) {
+        }
+        else if (ch == 2)
+        {
+            int x;
+            cout << "Enter value to insert at tail: ";
+            cin >> x;
+            insertend(x);
+        }
+        else if (ch == 3)
+        {
             // Insert at position logic can be added here
-        } else if (ch == 4) {
+        }
+        else if (ch == 4)
+        {
             // Delete at head logic can be added here
-        } else if (ch == 5) {
+        }
+        else if (ch == 5)
+        {
             // Delete at tail logic can be added here
-        } else if (ch == 6) {
+        }
+        else if (ch == 6)
+        {
             // Delete at position logic can be added here
-        } else if (ch == 7) {
+        }
+        else if (ch == 7)
+        {
             // Search logic can be added here
-        } else if (ch == 8) {
+        }
+        else if (ch == 8)
+        {
             printlist();
             cout << endl;
-        } else if (ch == 9) {
+        }
+        else if (ch == 9)
+        {
             break; // Exit the loop
-        } else {
+        }
+        else
+        {
             cout << "Invalid choice, please try again." << endl;
         }
-    
-    
     }
-
+    cout << "Exiting program." << endl;
     printlist();
-    cout << endl;
-return 0;
+    return 0;
 }
