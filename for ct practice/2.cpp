573537b9c489df422d28b3dd0110cd24d911239a
MIR-RIAZUL <mirriazul859@gmail.com>
2025-08-25 21:57:55 +0600

    vhjdsfg

--- for ct practice/2.cpp
+++ for ct practice/2.cpp
@@ -4,37 +4,64 @@ using namespace std;
 struct node
 {
     int data;
-    node* next;
+    node *next;
 };
-node* head=NULL;
+node *head = NULL;
 
-node* createnode(int num)
+node *createnode(int num)
 {
-    node* newnode =new node();
-    newnode->data=num;
-    newnode->next=NULL;
+    node *newnode = new node();
+    newnode->data = num;
+    newnode->next = NULL;
     return newnode;
-    
 }
 
 void inserthead(int num)
 {
-    node* newnode = createnode(num);
-    newnode->next=head;
-    head=newnode;
-
+    node *newnode = createnode(num);
+    newnode->next = head;
+    head = newnode;
 }
 void print()
 {
-    node* temp= head;
-    while(temp!=NULL)
+    node *temp = head;
+    while (temp != NULL)
     {
-        cout<<temp->data<<"->"; // Fixed: prints values in a single line with arrows
-        temp= temp->next;
+        cout << temp->data << "->"; // Fixed: prints values in a single line with arrows
+        temp = temp->next;
     }
-    cout<<"NULL"<<endl;
+    cout << "NULL" << endl;
 }
 
+void insertTail(int x)
+{
+    node *newnode = createnode(x);
+
+    node *temp = head;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    newnode->next = temp->next;
+    temp->next = newnode;
+}
+void insert_nth_node(int pos,int x)
+{
+    node* newnode=createnode(x);
+    node* temp=head;
+
+    for(int i =1;i<pos;i++)
+    {
+        temp=temp->next;
+    }
+    newnode->next = temp->next;
+    temp->next = newnode;
+}
+
+void after_value(int x,int val)
+{
+     b   
+}
 
 
 void choice()
@@ -61,7 +88,6 @@ void choice()
     cout << endl;
 }
 
-
 int main()
 {
     while (true)
@@ -74,69 +100,68 @@ int main()
         if (ch == 1)
         {
             int num;
-            cout<<"enter your number:"<<endl;
-            cin>>num;
-            inserthead(num);  
+            cout << "enter your number:" << endl;
+            cin >> num;
+            inserthead(num);
         }
         else if (ch == 2)
         {
-        
+
+            int num;
+            cout << "enter your number:" << endl;
+            cin >> num;
+            insertTail(num);
         }
         else if (ch == 3)
         {
-         
+             int num;
+             int pos;
+            cout << "enter your number:" << endl;
+            cin >> num;
+            cout<<"enter yout position that you =want to add"<<endl;
+            cin>>pos;
+            insert_nth_node( pos,num);
+    
         }
         else if (ch == 33)
         {
-     
         }
         else if (ch == 4)
         {
-        
         }
         else if (ch == 5)
         {
-       
         }
         else if (ch == 6)
         {
-            
         }
         else if (ch == 66)
         {
-            
         }
         else if (ch == 7)
         {
-            
         }
         else if (ch == 8)
         {
-          print();
+            print();
         }
         else if (ch == 88)
         {
-         
         }
         else if (ch == 9)
         {
-         
         }
         else if (ch == 10)
         {
-           
         }
         else if (ch == 11)
         {
-         
         }
         else if (ch == 12)
         {
- 
         }
         else if (ch == 13)
         {
-      
         }
 
         else if (ch == 0)
